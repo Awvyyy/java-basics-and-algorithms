2b21a8452a779f956d865aa51f6b7ac05c074c7a
Awvyyy <153191099+Awvyyy@users.noreply.github.com>
2025-12-08 01:06:50 +0200

    Fizz Buzz excercice

---
+++ src/FizzBuzz.java
@@ -0,0 +1,27 @@
+// if number is dividible by 3 - print Fizz
+// if number is dividible by 5 - print Buzz
+// if number is dividible by 3 and 5 - print FizzBuzz
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
+
+        scanner.close();
+    }
+}
