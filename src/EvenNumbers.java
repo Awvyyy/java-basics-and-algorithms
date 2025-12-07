0ed936194787e74bfcd96e792582e79ce14b9db7
Awvyyy <153191099+Awvyyy@users.noreply.github.com>
2025-12-08 01:09:08 +0200

    Even numbers excercice

---
+++ src/EvenNumbers.java
@@ -0,0 +1,20 @@
+// Print only even numbers from 1 to N
+
+
+import java.util.Scanner;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter N:");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+
+        input.close();
+    }
+}
