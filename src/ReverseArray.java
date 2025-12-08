7476656edd3c8527d339a699a421b8975786e037
awvyyy <helpned1@gmail.com>
2025-12-09 01:00:03 +0200

    Reverse array in place exercice

---
+++ src/ReverseArray.java
@@ -0,0 +1,32 @@
+// Reverse array in place
+
+
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] num = {31, 24, 0, 4, 6, 17};
+
+        System.out.print("Before: ");
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+
+        int left = 0;
+        int right = num.length - 1;
+
+        while (left < right) {
+            int temp = num[left];
+            num[left] = num[right];
+            num[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println();
+        System.out.print("After:  ");
+        for (int n : num) {
+            System.out.print(n + " ");
+        }
+    }
+}
