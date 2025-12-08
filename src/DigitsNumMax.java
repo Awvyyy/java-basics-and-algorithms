22f9db3a42c6eb5612bc4e5e21fcab25a53f3a86
awvyyy <helpned1@gmail.com>
2025-12-08 18:01:24 +0200

    Find digits,digitSum and maxNumber exercise

---
+++ src/DigitsNumMax.java
@@ -0,0 +1,33 @@
+// find digit amount, digit sum and max num
+
+
+
+import java.util.Scanner;
+
+public class DigitsNumMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        int number = Math.abs(scanner.nextInt());
+        int digits = 0;
+        int digitSum = 0;
+        int maxNumber = 0;
+
+        while(number > 0)
+        {
+            int digit = number % 10;
+            digits++;
+            digitSum += digit;
+            number = number / 10;
+
+            if (digit > maxNumber)
+                maxNumber = digit;
+        }
+
+        System.out.println("Amount of digits: " + digits);
+        System.out.println("Sum of all digits: " + digitSum);
+        System.out.println("Max number: " + maxNumber);
+        scanner.close();
+    }
+}
