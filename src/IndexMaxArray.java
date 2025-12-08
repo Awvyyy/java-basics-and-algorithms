17c4804df0d0c06102a236ba4384607983c4ecbf
awvyyy <helpned1@gmail.com>
2025-12-09 00:58:22 +0200

    Find max and maxIndex exercise

---
+++ src/IndexMaxArray.java
@@ -0,0 +1,26 @@
+//Find max value and index of max value
+
+
+
+public class IndexMaxArray {
+    public static void main(String[] args) {
+        int[] num = {31, 24, 36, 4, 6, 17};
+        int max = num[0];
+        int maxIndex = 0;
+
+        System.out.print("Array: ");
+        for(int i = 0; i < num.length; i++)
+        {
+            System.out.print(num[i] + " | ");
+            if (num[i] > max)
+            {
+                max = num[i];
+                maxIndex = i;
+            }
+        }
+
+        System.out.println();
+        System.out.println("Max value: " + max);
+        System.out.println("Max index: " + maxIndex);
+    }
+}
