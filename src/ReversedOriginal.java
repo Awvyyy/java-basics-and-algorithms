0ab900adc6a9a14f7a8c30215bc1d0f7a84f7bb6
awvyyy <helpned1@gmail.com>
2025-12-08 18:01:58 +0200

    reverse ><= original exercise

---
+++ src/ReversedOriginal.java
@@ -0,0 +1,34 @@
+// reverse number ><= original number
+
+
+
+import java.util.Scanner;
+
+public class ReversedOriginal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        int num = scanner.nextInt();
+        int original = num;
+        int reversed = 0;
+
+        while(num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+
+        if (original > reversed)
+            System.out.println(reversed + " is smaller than " + original);
+        else if (original < reversed)
+            System.out.println(reversed + " is bigger than " + original);
+        else if (original == reversed)
+            System.out.println(reversed + " and " + original + " are equal.");
+
+        scanner.close();
+    }
+}
+
