58611ef4be9e2e323c32d7a7046c69169155e004
awvyyy <helpned1@gmail.com>
2025-12-08 02:04:53 +0200

    Find smallest digit except 0 excercice

---
+++ src/SmallestDigit.java
@@ -0,0 +1,23 @@
+// Find the smallest digit in a number but skip zeros
+
+import java.util.Scanner;
+
+public class SmallestDigit {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = Math.abs(input.nextInt());
+        int min = 9;
+
+        while (num > 0) {
+            int digit = num % 10;
+            num = num / 10;
+
+            if (digit != 0 && min > digit)
+                min = digit;
+
+        }
+        System.out.println("Smallest digit except 0 is: " + min);
+}
+    }
